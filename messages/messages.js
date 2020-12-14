81d1a0ec7bf7b9af7aecc23b91936adf89bdacef
Almeida <Rafael_Almeida1@Dellteam.com>
2020-12-14 18:09:51 -0300

    fix message error

--- messages/messages.js
+++ messages/messages.js
@@ -1,4 +1,4 @@
 module.exports = {
-    GET_RANK: "Congratulations..You get a tope 10 rank..",
-    NOT_GET_RANK: "So sorry..You not get a tope 10 rank..See you in next time",
+    GET_RANK: "Congratulations..You get a top ten rank..",
+    NOT_GET_RANK: "So sorry..You not get a top ten rank..See you in next time",
   };
